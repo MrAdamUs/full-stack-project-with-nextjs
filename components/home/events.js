f172332553eabd1e6331231b4caa1426b2220454
Adam Mawlawi <adam@mradam.us>
2022-01-11 21:20:26 -0600

    update home page

--- components/home/events.js
+++ components/home/events.js
@@ -21,6 +21,75 @@ const Events = () => {
           </div>
         </div>
       </section>
+
+      <div id='home_events'>
+        <div className='container-fluid p-0'>
+          <div className='row g-0'>
+            <div className='col-lg-4 col-sm-6' key={`1`}>
+              <Link href={`/shows/the_show_slug`}>
+                <a className='event-box'>
+                  <Image
+                    src={`/images/venues/one.jpg`}
+                    alt={`alt of pic`}
+                    width='1920'
+                    height='1080'
+                    layout='responsive'
+                    className='img-fluid'
+                  />
+                  <div className='event-box-caption'>
+                    <div className='project-category text-white-50'>
+                      12-15-2021
+                    </div>
+                    <div className='project-name'>Bob's tavern</div>
+                  </div>
+                </a>
+              </Link>
+            </div>
+
+            <div className='col-lg-4 col-sm-6' key={`1`}>
+              <Link href={`/shows/the_show_slug`}>
+                <a className='event-box'>
+                  <Image
+                    src={`/images/venues/one.jpg`}
+                    alt={`alt of pic`}
+                    width='1920'
+                    height='1080'
+                    layout='responsive'
+                    className='img-fluid'
+                  />
+                  <div className='event-box-caption'>
+                    <div className='project-category text-white-50'>
+                      12-15-2021
+                    </div>
+                    <div className='project-name'>Bob's tavern</div>
+                  </div>
+                </a>
+              </Link>
+            </div>
+
+            <div className='col-lg-4 col-sm-6' key={`1`}>
+              <Link href={`/shows/the_show_slug`}>
+                <a className='event-box'>
+                  <Image
+                    src={`/images/venues/one.jpg`}
+                    alt={`alt of pic`}
+                    width='1920'
+                    height='1080'
+                    layout='responsive'
+                    className='img-fluid'
+                  />
+                  <div className='event-box-caption'>
+                    <div className='project-category text-white-50'>
+                      12-15-2021
+                    </div>
+                    <div className='project-name'>Bob's tavern</div>
+                  </div>
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
